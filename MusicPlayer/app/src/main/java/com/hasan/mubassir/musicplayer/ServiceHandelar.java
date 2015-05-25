7811aeff02e5b9a4e91cf7efa56363f36b74f8f8
Mubassir Hasan <mubassir913@gmail.com>
2015-05-26 01:20:53 +0600

    Initial commit

---
+++ MusicPlayer/app/src/main/java/com/hasan/mubassir/musicplayer/ServiceHandelar.java
@@ -0,0 +1,15 @@
+package com.hasan.mubassir.musicplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Mubassir on 26/05/2015 0026.
+ */
+public class ServiceHandelar extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
